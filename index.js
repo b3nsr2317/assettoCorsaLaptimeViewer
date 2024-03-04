58aaedfd09acb1d3ba6e096c4ef359f9cc34c180
Ben Robertson <ben.robertson04@gmail.com>
2024-03-04 22:35:46 +0000

    get more details and display title row

--- index.js
+++ index.js
@@ -20,14 +20,19 @@ const fileToJson = () => {
 };
 
 const updateTable = (json) => {
+    // get and clear table
+    const table = document.querySelector("#laptime-table");
+    table.innerHTML = "";
     // get all necessary information to display
     const player = json.players[0];
     const session = json.sessions[0];
     const laps = session.laps;
     const bestLap = session.bestLaps[0].lap;
+    const sectors = laps[0].sectors.length - 1;
     const car = player.car.replace(/\_/g, " ");
     const name = player.name;
     const eventType = session.name;
+    // get session details as h5 html
     const sessionDetails =
         "<h5>Player: " +
         name +
@@ -36,5 +41,14 @@ const updateTable = (json) => {
         " - Event: " +
         eventType +
         "</h5>";
+    // display session details
     document.querySelector("#session-details").innerHTML = sessionDetails;
+    // get and display title row
+    let titleRow =
+        "<tr><th style='width: 150px;'>Lap Number</th><th>Lap Time</th>";
+    for (let i = 0; i <= sectors; i++) {
+        titleRow += "<th>Sector " + (i + 1) + "</th>";
+    }
+    titleRow += "<th>Cuts</th></tr>";
+    table.innerHTML += titleRow;
 };
