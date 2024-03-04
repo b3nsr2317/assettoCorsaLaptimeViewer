f34891abf291e1ea437b4707188cc83126f482a8
Ben Robertson <ben.robertson04@gmail.com>
2024-03-04 22:22:01 +0000

    read file and get some information

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const file = document.querySelector("#file-upload");
-file.addEventListener("change", () => {
+file.value = "";
+file.addEventListener("change", (event) => {
     const pattern = /.*\.json/;
     if (pattern.test(file.value)) {
-        console.log("file ok");
+        fileToJson(event);
     } else {
         file.value = "";
     }
 });
+
+const fileToJson = () => {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const contents = event.target.result;
+        const json = JSON.parse(contents);
+        updateTable(json);
+    };
+    reader.readAsText(file.files[0]);
+};
+
+const updateTable = (json) => {
+    // get all necessary information to display
+    const player = json.players[0];
+    const session = json.sessions[0];
+    const laps = session.laps;
+    const bestLap = session.bestLaps[0].lap;
+    const car = player.car.replace(/\_/g, " ");
+    const name = player.name;
+    const eventType = session.name;
+    const sessionDetails =
+        "<h5>Player: " +
+        name +
+        " - Car: " +
+        car +
+        " - Event: " +
+        eventType +
+        "</h5>";
+    document.querySelector("#session-details").innerHTML = sessionDetails;
+};
